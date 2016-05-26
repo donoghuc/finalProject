dda0f341ffe6f84116f5da7e839c171f90ea7fb2
donoghuc <cas.donoghue@gmail.com>
2016-05-25 20:09:56 -0700

    /insert INTERESTING
    looking in to why the headers dont match the data in table. Looking in
    to /insert order of insertion

--- helloMysql.js
+++ helloMysql.js
@@ -36,8 +36,8 @@ app.get('/',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name,
-    req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`lbs`,`date`) VALUES (?,?,?,?,?)", [req.query.name,
+    req.query.reps, req.query.weight, req.query.lbs, req.query.date], function(err, result){
     if(err){
       next(err);
       return;
